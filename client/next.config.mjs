48d7e91e1a544ccee5e2a221fe6531d46334e67f
Yusuf <yshaikh113@outlook.com>
2024-11-13 10:36:29 -0600

    updated next config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        //protocol: "https",
-        //hostname: 
-        //port: "",
-        //pathname: "/**",
-      }
-    ]
-  }
-};
-
+const nextConfig = {};
 export default nextConfig;
